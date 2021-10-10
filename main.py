050ff69735ee17b0598ac08b51fd30e2bf42668c
Cardos Vasile <cardosvasile15@gmail.com>
2021-10-10 15:46:15 +0200

    lab 3

---
+++ main.py
@@ -0,0 +1,11 @@
+def i(n):
+    if n>2:
+        return 2
+    else:
+        return 3
+def main():
+    p=int(input('numarul mwahahahaha: '))
+    t=i(p)
+    print(f'fawful {t}')
+if __name__ == '__main__':
+    main()
