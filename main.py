4b740a1b81a2e9e1421385b36bce23681dc79e6f
Cardos Vasile <cardosvasile15@gmail.com>
2021-10-12 12:59:19 +0200

    lab 3

--- main.py
+++ main.py
@@ -7,6 +7,39 @@ def read_list():
     return lst
 
 
+def is_palindrom(n):
+    aux=n
+    inv=0
+    if n >= 10:
+        while n:
+            inv=inv*10+n%10
+            n=n//10
+    else:
+        return False
+
+    if aux == inv:
+        return True
+    else:
+        return False
+
+
+def get_longest_all_palindromes(lst: list[int]):
+    n = len(lst)
+    res = []
+    for s in range(n):
+        for d in range(s, n):
+            palindrom = True
+            for num in lst[s:d+1]:
+                if is_palindrom(num) == False:
+                    palindrom = False
+                    break
+            if palindrom:
+                if d-s+1>len(res):
+                    res=lst[s:d+1]
+
+    return res
+
+
 
 
 
@@ -18,14 +51,15 @@ def main():
     lst = []
     while True:
         print('1. Citire lista: ')
-        print('2. Optiunea 2')
+        print('2. Cea mai lunga subsecventa de palindroame')
         print('3. Optiunea 3')
         print('x. Exit')
         optiune = input('Optiunea: ')
         if optiune == '1':
             lst = read_list()
         elif optiune == '2':
-            pass
+            palindrom = get_longest_all_palindromes(lst)
+            print(palindrom)
         elif optiune == '3':
             pass
         elif optiune == 'x':
@@ -34,7 +68,5 @@ def main():
             print('Optiune invalida')
 
 
-
-
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
